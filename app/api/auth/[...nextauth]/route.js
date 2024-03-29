b1acaf86a22fc0488e1e01d5d6ccfdd01abbefd3
ShivaKanchi <shivakanchi111@gmail.com>
2024-03-29 23:22:51 +0530

    Creating User Model

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,40 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import { connectDB } from "@utils/database";
+import User from "@model/user";
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: "",
-      clientSecret: "",
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   async session({ session }) {},
-  async signIn({ profile }) {},
+  async signIn({ profile }) {
+    try {
+      // serverless ->Lambda ->dynamodb
+      await connectDB();
+
+      // Check if user already exist
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+      // IF user new then create new
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 });
 
 export { handler as GET, handler as POST };
---
+++ utils/database.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+let isConnected = fasle;
+
+export const connectDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("DB already Connected");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "talkcreek",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log("DB Connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
