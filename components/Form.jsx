adad34a32f74490463947b9988770a717e3ef967
ShivaKanchi <shivakanchi111@gmail.com>
2024-03-30 23:12:26 +0530

    working on form

--- components/Form.jsx
+++ components/Form.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <section className="w-full max-w-full flex -start">
+    <section className="w-full max-w-full flex-start flex-col">
       <h1 className="head_text text-left">
         <span className="blue_gradient">{type} Post</span>
       </h1>
@@ -15,8 +15,8 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
         className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
       >
         <label>
-          <span className="font-satoshi font-semibold   text-base text-gray-700">
-            Your Topic
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Your Topic:
           </span>
           <textarea
             value={post.talk}
@@ -29,12 +29,12 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
 
         <label>
           <span className="font-satoshi font-semibold   text-base text-gray-700">
-            Tag <span>(#politics, #india)</span>
+            Tag:
           </span>
           <input
             value={post.tag}
             onChange={(e) => setPost({ ...post, tag: e.target.value })}
-            placeholder="Mention the tag"
+            placeholder="#price, #india, #webdev"
             required
             className="form_input"
           />
