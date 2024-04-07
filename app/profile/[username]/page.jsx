fc4fc0f2a0876f1a2567ef3de32ca202e6d6855f
ShivaKanchi <shivakanchi111@gmail.com>
2024-04-07 22:45:05 +0530

    Solving hosting issues

--- app/profile/[username]/page.jsx
+++ app/profile/[username]/page.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+"use client";
+import { useSession } from "next-auth/react";
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Profile from "@components/Profile";
 
 const UserProfile = () => {
-  return <div>UserProfile</div>;
+  const { data: session } = useSession();
+
+  const [posts, setPosts] = useState([]);
+
+  const router = useRouter();
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    };
+    if (session?.user.id) fetchPosts();
+    console.log("Data fetch for profile", session, posts);
+  }, []);
+  const handleEdit = (post) => {
+    router.push(`/update-talk?id=${post._id}`);
+  };
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/talk/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((p) => p._id !== post.id);
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+  return (
+    <Profile
+      name={username}
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 };
 
 export default UserProfile;
